0ddc35b66f9837685f6cf3fb44b8e800ef0e7e18
ankit8 <ankitbummerkar8@gmail.com>
2026-07-29 17:52:46 +0530

    second

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ app.use(
   }),
 );
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
