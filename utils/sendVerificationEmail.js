16fbfda79c4942a18c21fe0f1c2d72c0b673bac3
ankit8 <ankitbummerkar8@gmail.com>
2026-07-30 11:44:22 +0530

    Use Gmail SMTP port 587

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -2,7 +2,10 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log("SMTP_HOST =", process.env.SMTP_HOST);
+console.log("SMTP_PORT =", process.env.SMTP_PORT);
+console.log("SMTP_USER =", process.env.SMTP_USER);
+console.log("SMTP_PASS exists =", !!process.env.SMTP_PASS);
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
